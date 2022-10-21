0756f0d6edee9af118d05b7ef3b0b03b449501ef
chaeaw04 <chaeaw04@gmail.com>
2022-10-21 15:50:30 +0900

    feat : get, show DATA

--- practice/script.js
+++ practice/script.js
@@ -4,4 +4,38 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const $posts = get('.posts')
+  const getPost = async () => {
+    const API_URL = 'https://jsonplaceholder.typicode.com/posts'
+
+    const response = await fetch(API_URL)
+    if (!response.ok) {
+      throw new Error('에러가 발생했습니다.')
+    }
+    return await response.json()
+  }
+
+  const showPost = (posts) => {
+    posts.forEach((post) => {
+      const $post = document.createElement('div')
+      $post.classList.add('post')
+      $post.innerHTML = `
+      <div class="header">
+        <div class="id">${post.id}</div>
+        <div class="title">${post.title}</div>
+        </div>
+      <div class="body">
+        ${post.body}
+      </div>`
+      $posts.appendChild($post)
+    })
+  }
+  const loadPost = async () => {
+    const response = await getPost()
+    showPost(response)
+  }
+  window.addEventListener('DOMContentLoaded', () => {
+    loadPost()
+  })
 })()
